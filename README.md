ca5e3d1200ebb2f7315ef0a498e90990cc26d4d5
Kane York <rikingcoding@gmail.com>
2019-11-16 18:26:29 -0800

    Licensing & comment cleanups; create vec.go for clarity on licensing

---
+++ README.md
@@ -0,0 +1,8 @@
+# go-prometheus-topk
+
+A custom Prometheus metric exporter wrapping the github.com/dgryski/go-topk library.
+
+By using top-K metric collection, you can estimate the outliers of
+high-cardinality data without storing the entire dataset. (If you need
+something more precise than "estimates", then structured log processing is the
+way to go.)
--- prometheus.go
+++ prometheus.go
@@ -1,5 +1,6 @@
 /*
-Copyright 2019 Kane York, Google LLC
+Copyright 2019 Kane York
+Copyright 2019 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -92,6 +93,7 @@ type TopKOpts struct {
 }
 
 type topkRoot struct {
+	// unfortunately, all access to the Stream needs to be protected
 	streamMtx sync.Mutex
 	stream    *tk.Stream
 
@@ -99,7 +101,6 @@ type topkRoot struct {
 	errDesc   *prometheus.Desc
 
 	variableLabels []string
-	writeMtx       sync.Mutex // Only used in Write method.
 }
 
 type curriedLabelValue struct {
@@ -133,7 +134,7 @@ var (
 func NewTopK(opts TopKOpts, labelNames []string) TopK {
 	fqName := prometheus.BuildFQName(opts.Namespace, opts.Subsystem, opts.Name)
 
-	// Take a copy to avoid shenanigans
+	// Take a copy to avoid mutation
 	varLabels := append([]string(nil), labelNames...)
 
 	root := &topkRoot{
@@ -182,153 +183,3 @@ func (b *topkWithLabelValues) Inc() {
 	b.Observe(1)
 }
 
-// note: label manipulation copied heavily from prometheus/client_golang/prometheus/vec.go
-
-// MustCurryWith implements the Vec interface.
-func (r *topkCurry) MustCurryWith(labels prometheus.Labels) TopK {
-	n, err := r.CurryWith(labels)
-	if err != nil {
-		panic(err)
-	}
-	return n
-}
-
-// CurryWith implements the Vec interface.
-func (r *topkCurry) CurryWith(labels prometheus.Labels) (TopK, error) {
-	var (
-		newCurry []curriedLabelValue
-		oldCurry = r.curry
-		iCurry   int
-	)
-	for i, label := range r.root.variableLabels {
-		val, ok := labels[label]
-		if iCurry < len(oldCurry) && oldCurry[iCurry].index == i {
-			if ok {
-				return nil, fmt.Errorf("label name %q is already curried", label)
-			}
-			newCurry = append(newCurry, oldCurry[iCurry])
-			iCurry++
-		} else {
-			if !ok {
-				continue // Label stays unset
-			}
-			newCurry = append(newCurry, curriedLabelValue{i, val})
-		}
-	}
-	if leftover := len(oldCurry) + len(labels) - len(newCurry); leftover > 0 {
-		return nil, fmt.Errorf("%d unknown label(s) found during currying", leftover)
-	}
-
-	return &topkCurry{
-		curry: newCurry,
-		root:  r.root,
-	}, nil
-}
-
-func (r *topkCurry) compositeWithLabels(labels prometheus.Labels) (string, error) {
-	if err := validateLabels(labels, len(r.root.variableLabels)-len(r.curry)); err != nil {
-		return "", err
-	}
-
-	var (
-		keyBuf bytes.Buffer
-		curry  = r.curry
-		iCurry int
-	)
-
-	for i, label := range r.root.variableLabels {
-		val, ok := labels[label]
-		if iCurry < len(curry) && curry[iCurry].index == i {
-			if ok {
-				return "", fmt.Errorf("label name %q is already curried", label)
-			}
-			keyBuf.WriteString(curry[iCurry].value)
-			iCurry++
-		} else {
-			if !ok {
-				return "", fmt.Errorf("label name %q missing in label map", label)
-			}
-			keyBuf.WriteString(val)
-		}
-		keyBuf.WriteByte(model.SeparatorByte)
-	}
-	return keyBuf.String(), nil
-}
-
-func (r *topkCurry) compositeWithLabelValues(lvs ...string) (string, error) {
-	if err := validateLabelValues(lvs, len(r.root.variableLabels)-len(r.curry)); err != nil {
-		return "", err
-	}
-
-	var (
-		keyBuf        bytes.Buffer
-		curry         = r.curry
-		iVals, iCurry int
-	)
-	for i := 0; i < len(r.root.variableLabels); i++ {
-		if iCurry < len(curry) && curry[iCurry].index == i {
-			keyBuf.WriteString(curry[iCurry].value)
-			iCurry++
-		} else {
-			keyBuf.WriteString(lvs[iVals])
-			iVals++
-		}
-		keyBuf.WriteByte(model.SeparatorByte)
-	}
-
-	return keyBuf.String(), nil
-}
-
-func validateLabels(labels prometheus.Labels, expectCount int) error {
-	if expectCount > len(labels) {
-		return fmt.Errorf("not enough labels (missing %d)", expectCount-len(labels))
-	} else if expectCount < len(labels) {
-		return fmt.Errorf("received %d unrecognized labels", len(labels)-expectCount)
-	}
-	return nil
-}
-
-func validateLabelValues(lvs []string, expectCount int) error {
-	if expectCount > len(lvs) {
-		return fmt.Errorf("not enough labels (missing %d)", expectCount-len(lvs))
-	} else if expectCount < len(lvs) {
-		return fmt.Errorf("received %d unrecognized labels", len(lvs)-expectCount)
-	}
-	return nil
-}
-
-// GetMetricWith implements the Vec interface.
-func (r *topkCurry) GetMetricWith(labels prometheus.Labels) (TopKBucket, error) {
-	composite, err := r.compositeWithLabels(labels)
-	if err != nil {
-		return nil, err
-	}
-	return &topkWithLabelValues{compositeLabel: composite, root: r.root}, nil
-}
-
-// With implements the Vec interface.
-func (r *topkCurry) With(labels prometheus.Labels) TopKBucket {
-	composite, err := r.compositeWithLabels(labels)
-	if err != nil {
-		panic(err)
-	}
-	return &topkWithLabelValues{compositeLabel: composite, root: r.root}
-}
-
-// GetMetricWithLabelValues implements the Vec interface.
-func (r *topkCurry) GetMetricWithLabelValues(lvs ...string) (TopKBucket, error) {
-	composite, err := r.compositeWithLabelValues(lvs...)
-	if err != nil {
-		return nil, err
-	}
-	return &topkWithLabelValues{compositeLabel: composite, root: r.root}, nil
-}
-
-// WithLabelValues implements the Vec interface.
-func (r *topkCurry) WithLabelValues(lvs ...string) TopKBucket {
-	composite, err := r.compositeWithLabelValues(lvs...)
-	if err != nil {
-		panic(err)
-	}
-	return &topkWithLabelValues{compositeLabel: composite, root: r.root}
-}
